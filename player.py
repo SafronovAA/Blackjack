15348334d5bc5793acd7a85785028a16f816b599
Artem Safronov <joneletii@gmail.com>
2022-10-02 19:39:18 +0500

    added methods in player.py

---
+++ player.py
@@ -0,0 +1,22 @@
+from tempfile import gettempdir
+from card import Card
+
+
+class Player():
+    def __init__(self, name: str) -> None:
+        self.name = name
+        self.points = 0
+        self.cards = []
+
+    def add_card(self, card: Card):
+        self.cards.append(card)
+
+    @property
+    def add_points(self, points: int):
+        self.points += points
+
+    @add_points.getter
+    def points(self):
+        return self.points
+
+    
