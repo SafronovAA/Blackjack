a44ff916758bd4f5508a24415987dc74f2f8f0ed
Artem Safronov <joneletii@gmail.com>
2022-10-02 14:38:27 +0500

    added class Deck in deck.py

--- deck.py
+++ deck.py
@@ -1,3 +1,28 @@
+from itertools import product
+from random import shuffle
+
+from const import SUITS, RANKS
+from card import Card
 
 
-    
+class Deck():
+    def __init__(self) -> None:
+        self.cards = self.create_deck()
+        shuffle(self.cards)
+
+    def create_deck(self):
+        cards = []
+        points = 0
+        for suit, rank in product(SUITS, RANKS):
+            if rank == 'валет':
+                    points = 2
+            elif rank == 'дама':
+                    points = 3
+            elif rank == 'король':
+                    points = 4
+            elif rank == 'туз':
+                    points = 10
+            else:
+                point= int(rank)
+            cards.append(Card(suit=suit, rank=rank, points=points))
+        return cards
