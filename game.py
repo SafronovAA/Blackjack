1996467e80017ee853c524c81313631f98cc0d4e
Artem Safronov <joneletii@gmail.com>
2022-10-02 20:30:20 +0500

    added methods in game.py

--- game.py
+++ game.py
@@ -3,15 +3,32 @@ from deck import Deck
 
 class Game():
     def __init__(self) -> None:
-        self.players = []
+        self.player = self.add_player()
         self.deck = Deck()
+        self.dealer = self.add_dealer()
 
     def add_player(self, player: str) -> Player:
         player = Player(name = player)
-        self.players.append(player)
         return player
+
+    def add_dealer(self):
+        dealer = Player(name = "Дилер")
+        return dealer
     
     def get_card(self):
         return self.deck.pop_card()
 
+    def ask_question(text: str):
+        answer = input(text)
+        if answer == "y":
+            return True
+        else:
+            return False
     
+    def start_game(self):
+        answer = self.ask_question("Вы хотите начать игру?")
+        if answer:
+            self.player.add_card(self.deck.pop_card())
+            
+
+
